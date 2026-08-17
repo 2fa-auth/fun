ede408501726c4a272265ffd6e0dda73e0064f1a
2fa-auth <sir.jey.daniels@gmail.com>
2026-08-18 02:43:41 +0300

    add a description of an existing function

--- noob/phase_1/gradient.py
+++ noob/phase_1/gradient.py
@@ -1,12 +1,25 @@
 #!/home/client/Documents/fun/py/venv/bin/python3
 import numpy as np
 
-def train_step(x, target, p, learning_rate):
+
+def train_step(X, target, p, learning_rate):
+  """
+  функция вычисляет:
+    loss: L = (y-t)**2
+      производная: dL/dy = 2(y-t)
+    y: y = px
+      производная: dy/dp = x
+    backprop:
+      dL/dp = dL/dy * dy/dp
+    optimization:
+      p_new = p - lr * dL/dp 
+  """
+
   # FORWARD
-  y_pred = x @ p 
+  y_pred = X @ p 
   loss = (y_pred - target) ** 2 # shape = (4,)
   # BACKWARD
-  grad_p = (2/len(x)) * (y_pred - target) @ x # shape = (2)
+  grad_p = (2/len(X)) * (y_pred - target) @ X # shape = (2)
   # UPDATE
   p -= learning_rate * grad_p # shape = (2,)
 
@@ -19,7 +32,6 @@ def main():
     [3., 1.],
     [5., 2.]
   ])
-
   p = np.array([1., 2.])
 
   target = np.array([
