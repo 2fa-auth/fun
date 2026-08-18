fix bugs; add transpose (broadcasting)

noob/phase_1/gradient.py
@@ -1,19 +1,23 @@
 #!/home/client/Documents/fun/py/venv/bin/python3
 import numpy as np
 
-""" forward ==> barkward ==> optimization ==> again ..."""
+""" 
+базовая вычислительная архитектура градиента с нуля:
+  forward ==> barkward ==> optimization ==> again ...
+"""
 
 class Linear:
-  def __init__(self, in_features):
-    self.p = np.random.randn(in_features)
-    self.b = np.random.randn()
+  def __init__(self, in_features, out_features):
+    self.p = np.random.randn(in_features, out_features)
+    self.b = np.random.randn(out_features)
 
   def forward(self, X):
     return X @ self.p + self.b
 
   def backward(self, grad_output, X):
-    grad_p = grad_output @ X
-    grad_b = grad_output.sum()
+    X = X.T # (4, 2) => (2, 4) (по правилу broadcasting)
+    grad_p = X @ grad_output 
+    grad_b = grad_output.sum(axis=0)
 
     return grad_p, grad_b
 
@@ -31,22 +35,22 @@ def main():
   ])
 
   target = np.array([
-    10.,
-    9.,
-    7.,
-    13.
+    [10., 5., 2.],
+    [9.,  4., 3.],
+    [7.,  8., 1.],
+    [13., 6., 4.]
   ])
 
-  layer = Linear(2)
+  layer = Linear(2,3)
 
   for step in range(1000):
 
-    y_pred = layer.forward(X)
+    pred = layer.forward(X)
 
-    loss = ((y_pred - target)**2).mean() 
-    grad_output = 2 * (y_pred - target) / len(X)
+    loss = ((pred - target)**2).mean() 
+    grad_output = 2 * (pred - target) / len(X)
     grad_p, grad_b = layer.backward(grad_output, X)
-    layer.update(grad_p, grad_b, 0.01)
+    layer.optimization(grad_p, grad_b, 0.01)
 
     if step % 100 == 0:
       print(step, loss)