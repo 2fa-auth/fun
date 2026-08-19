d3eb679f58b9f91012ced77e23b2e0c9a1ead46f
2fa-auth <sir.jey.daniels@gmail.com>
2026-08-19 20:38:14 +0300

    analysis of gradient descent calculation for all parameters (noob level)

---
+++ noob/phase_1/linear_backward.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def relu(z):
+  return 0 if z < 0 else z
+
+x = 2.
+w = 3.
+b = 1
+
+def forward():
+  z = w*x+b # z = 7
+  a = relu(z)
+  L = a**2
+  return z, a, L
+
+z, a, L = forward()
+
+def backward():
+  """цель: вычислить dL / dx, dL / dw, dL / db"""
+  grad_a = 2*a               # dL / da
+  grad_z = 0 if z < 0 else 1 # da / dz
+  grad_w = x                 # dz / dw 
+  grad_x = w                 # dz / dx
+  grad_b = 1                 # dz / db
+
+  grad_x = grad_a * grad_z * grad_x # dL / dx
+  grad_W = grad_a * grad_z * grad_w # dL / dw
+  grad_bias = grad_a * grad_z * grad_b # dL / db
+
+  return grad_x, grad_W, grad_bias
+
+grad_x, grad_W, grad_bias = backward()
+print(grad_x, grad_W, grad_bias) # 42, 28, 14
--- noob/phase_1/mlp_v2.py
+++ noob/phase_1/mlp_v2.py
@@ -1,51 +1,19 @@
 #!/home/client/Documents/fun/py/venv/bin/python3
 import numpy as np
 
-
-class MSELoss:
-  def __call__(self, pred, target):
-    return ((pred - target)**2).mean()
-
-
-  
 class Linear:
-  def __init__(self, in_dim=1, out_dim=1):
+  def __init__(self, in_dim, out_dim):
     self.W = np.random.randn(in_dim, out_dim)    
     self.b = np.random.randn(out_dim)
-    self.grad_W = np.array([])
-    self.grad_b = np.array([])
 
   def forward(self, X):
     return X @ self.W + self.b
 
-  def backward(self, grad_output, X):
-    print(f'grad_output = {grad_output.shape}')
-    print(f'X = {X.shape}')
-    print()
-    grad_W = X @ grad_output
-    grad_b = grad_output.sum()
-
-    self.grad_W = grad_W
-    self.grad_b = grad_b
-
-    return grad_output
-     
-class OptimMLP(Linear):
-  def __init__(self, lr):
-    super().__init__()
-    self.lr = lr
-
-  def optimizer(self):
-    print(f'OLD self.W: \n{self.W}')
-    print(f'изменится НА \n{self.grad_W}')
-    print(f'OLD self.b = \n{self.b}')
-    print(f'изменится НА \n{self.grad_b}')
-
-    self.W -= self.lr * self.grad_W
-    self.b -= self.lr * self.grad_b
-
-    print(f'NEW self.W = \n{self.W}')
-    print(f'NEW self.b = \n{self.b}')
+  def backward(self):
+    pass
+  
+  def update(self):
+    pass
 
 class ReLU:
   def forward(self, X):
@@ -60,19 +28,18 @@ class MLP:
     self.relu = ReLU()
     self.fc2 = Linear(3, 1)
     
-  def forward(self, X):
-    X = self.relu.forward(self.fc1.forward(X))
-    X = self.fc2.forward(X)
-    return X # = > prediction of model
+  def forward(self):
+    pass
 
-  # backpropogation
-  def backward(self, grad_loss, X):
-    grad_output = self.relu.backward(self.fc2.backward(grad_loss, X), X)
-    grad_output = self.fc1.backward(grad_output, X)
+  def backward(self):
+    pass
 
+
+def mseloss(pred, target):
+    return ((pred - target)**2).mean()
 
 def main():
-  X = np.array([
+  X = np.array([ # (8, 2)
     [0., 0.],
     [0., 1.],
     [1., 0.],
@@ -82,9 +49,7 @@ def main():
     [2., 2.],
     [3., 1.]
   ])
-  print(f'X.shape = {X.shape}') # (8, 2)
-
-  target = np.array([
+  target = np.array([ # (8,)
     0.,
     3.,
     2.,
@@ -93,33 +58,12 @@ def main():
     4.,
     6.,
     7.
-  ])
-  print(f'target.shape = {target.shape}') # (8,)
-  print()
+  ]) 
   # после обучения предсказания должны быть близки к:
   # [0, 3, 2, 5, 7, 4, 6, 7]
 
   model = MLP()
 
-  criterion = MSELoss()
-  optim = OptimMLP(0.01)
- 
-  pred = model.forward(X)
-  
-  # получи итоговое лин. преобразование
-  loss = criterion(pred, target)
-  # среднюю ошибку
-  grad_loss = 2 * (pred - target) / len(X)
-  # градиент для всех target[i] для каждого X[j] (grad_loss = (8, 8))
-  
-  model.backward(grad_loss, X)
-  # посчитай для всех градиент (а не только для dL/dpred)
-
-  optim.optimizer()
-  # обнови значения
-
-  print(f'средняя ошибка = {loss}')
-  # выведи среднюю ошибку
 
 if __name__ == '__main__':
   main()
