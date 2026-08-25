376e3ccc5845f10e6c29f2eac3c16ae06e510d24
2fa-auth <sir.jey.daniels@gmail.com>
2026-08-25 13:15:01 +0300

    dropout implementation

--- noob/phase_2/main.py
+++ noob/phase_2/main.py
@@ -2,53 +2,135 @@
 import torch 
 import torch.nn as nn
 import torch.optim as optim
+import torch.utils.data as data
+
+class PetDataSet: # совместимый с torch.utils.data.Dataset
+  """
+  PetDataSet :
+    принимает объект и метку
+    возвращает количество примеров samples
+    по индексу возвращает соответствующую пару
+  """
+  def __init__(self, data, target):
+    self.data = data
+    self.target = target
+
+  def __getitem__(self, index):
+    return (self.data[index], self.target[index])
+
+  def __len__(self):
+    return self.data.size(0)
+
+def gen_size(data_size, test_size):
+  return int(data_size * test_size / 100)
+
+def gen_sets(train_arr, target_train_arr, target_test_arr, test_arr):
+  set_train = PetDataSet(train_arr, target_train_arr)
+  set_test = PetDataSet(test_arr, target_test_arr)
+  return set_train, set_test
+
+def gen_data(x, target, train_size):
+  train_arr = x[:train_size]
+  test_arr = x[train_size:]
+  target_train_arr = target[:train_size]
+  target_test_arr = target[train_size:]
+
+  set_train, set_test = gen_sets(train_arr, target_train_arr, test_arr, target_test_arr)
+
+  data_train = data.DataLoader(set_train, batch_size=32, shuffle=True)
+  data_test = data.DataLoader(set_test, batch_size=16, shuffle=False)
+  return (data_train, data_test)
 
 
-class Mod(nn.Module):
-  def __init__(self):
+# === DROPOUT ===
+# class MLP(nn.Module):
+#   def __init__(self, out_features_x, out_features_y):
+#     super().__init__()
+
+#     self.fc1 = nn.Linear(out_features_x, 16)
+#     self.fc2 = nn.Linear(16, 8)
+#     self.fc3 = nn.Linear(8, 4)
+#     self.fc4 = nn.Linear(4, out_features_y)
+#     self.relu = nn.ReLU()
+#     self.dropout = nn.Dropout(p=0.5)
+
+#   def forward(self, X):
+#     out = self.relu(self.fc1(X))
+#     out = self.relu(self.fc2(out))
+#     out = self.relu(self.fc3(out))
+#     out = self.fc4(out) 
+#     return out
+
+class MLP(nn.Module):
+  def __init__(self, out_features_x, out_features_y):
     super().__init__()
-    self.fc1 = nn.Linear(1, 8)
-    self.fc2 = nn.Linear(8, 4)
-    self.fc3 = nn.Linear(4, 1)
 
+    self.fc1 = nn.Linear(out_features_x, 16)
+    self.fc2 = nn.Linear(16, 8)
+    self.fc3 = nn.Linear(8, 4)
+    self.fc4 = nn.Linear(4, out_features_y)
     self.relu = nn.ReLU()
-  
+    self.dropout = nn.Dropout(p=0.5)
+
   def forward(self, X):
-    out = self.relu(self.fc1(X))
-    out = self.relu(self.fc2(out))
-    out = self.fc3(out)
+    out = self.dropout(self.relu(self.fc1(X)))
+    out = self.dropout(self.relu(self.fc2(out)))
+    out = self.dropout(self.relu(self.fc3(out)))
+    out = self.fc4(out) 
     return out
 
+
 def main():
-  X = torch.linspace(-2, 2, 100).unsqueeze(-1)
-  target = X ** 2
-
-  model = Mod()
-  opt = optim.SGD(model.parameters(), lr=0.01)
-  criterion = nn.MSELoss()
+  data_size = 100
   
-  for step in range(1000):
-    pred = model(X)
-    loss = criterion(pred, target)
-    opt.zero_grad()
-    loss.backward()
-    opt.step()
-    
-    if step % 100 == 0:
-      print(f"step = {step} | loss = {loss}")
-  print('train end\n')
+  test_size = gen_size(data_size, 30) # 30% тестовой выборке, а остальное обучающей
+  train_size = data_size - test_size
 
-  X = torch.linspace(-1, -3, 10).unsqueeze(-1)
-  target = X ** 2
-  print("real:")
-  print(f"X = {X}")
-  print(f"target = {target}")
-  print(f"\nprediction:")
-  with torch.no_grad():
-      pred = model(X)
-      print(f'X = {X}')
-      print(f'pred = {pred}')
+  x = torch.linspace(-5, 5, data_size).unsqueeze(-1)
+  target = x ** 2  
+
+  data_train, data_test = gen_data(x, target, train_size)
+
+  model = MLP(1, 1)
+
+  model.train()
+  pred1 = model(x)
+  pred2 = model(x)
+  print(f"режим train: pred1 {pred1[:2]} | pred2 {pred2[:2]}")
+
+  model.eval()
+  pred1 = model(x)
+  pred2 = model(x)
+  print(f"режим eval: pred1 {pred1[:2]} | pred2 {pred2[:2]}")
+
+  exit(0)
+
+  opt = optim.SGD(model.parameters(), lr=0.001)
+  criterion = nn.MSELoss()
+
+  num_ep = 1000
+
+  model.train()
+  for _ep in range(num_ep):
+    loss_mean = 0
+    loss_cnt = 0
+    for x, y in data_train:
+      pred = model(x)
+      loss = criterion(pred, y)
+      loss_mean += loss.item()
+      loss_cnt += 1
+
+      opt.zero_grad()
+      loss.backward()
+      opt.step()
+
+    if _ep % 50 == 0:
+      print(f'_ep = {_ep} | loss mean = {loss_mean}')
+c     
+  model.eval()
+
 
 if __name__ == "__main__":
   main()
+  
   
