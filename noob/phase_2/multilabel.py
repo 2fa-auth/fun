66abfccc581ffef8bd9bb54ffe8c09c544614647
2fa-auth <sir.jey.daniels@gmail.com>
2026-08-30 22:15:41 +0300

    add: test,validacion selection, raw valid loop

--- noob/phase_2/multilabel.py
+++ noob/phase_2/multilabel.py
@@ -4,6 +4,8 @@ import torch.utils.data as data
 import torch.nn as nn
 import torch.nn.functional as F
 
+from math import ceil, floor
+
 """
 многометочная классификация и метрики (КОСТЫЛЬ):
   * в этом файле отдается приоритет вычислению метрик.
@@ -58,7 +60,26 @@ def main():
     [0.5, 2.5, 1.0, 3.0],
     [3.0, 2.0, 2.0, 1.0],
     [1.0, 3.0, 0.5, 2.5],
-  ])
+    [2.5, 1.0, 1.5, 0.5],
+    [0.5, 1.5, 2.5, 2.0],
+    [3.5, 0.5, 1.0, 1.5],
+    [1.0, 2.5, 3.0, 0.5],
+    [2.0, 3.0, 0.5, 1.0],
+    [0.5, 0.5, 3.0, 2.5],
+    [3.0, 1.5, 2.5, 0.0],
+    [1.5, 2.0, 0.0, 3.0],
+    [2.5, 2.5, 1.0, 0.5],
+    [0.0, 3.0, 2.0, 1.0],
+    [3.0, 0.0, 1.5, 2.5],
+    [1.0, 1.0, 3.0, 2.0],
+    [2.2, 1.1, 2.8, 0.7],
+    [0.3, 2.8, 1.7, 2.2],
+    [3.2, 0.8, 0.9, 2.3],
+    [1.7, 2.7, 2.4, 0.3],
+    [2.8, 0.4, 3.1, 1.3],
+  ], dtype=torch.float32)
+
+
   target = torch.tensor([
     [1., 0., 1., 0., 0.],
     [0., 1., 1., 0., 1.],
@@ -68,10 +89,53 @@ def main():
     [0., 1., 0., 1., 1.],
     [1., 1., 1., 0., 0.],
     [0., 1., 0., 0., 1.],
-  ])
+    [1., 0., 1., 0., 0.],
+    [0., 1., 1., 1., 0.],
+    [1., 0., 0., 1., 1.],
+    [0., 1., 1., 0., 1.],
+    [1., 1., 0., 1., 0.],
+    [0., 0., 1., 1., 1.],
+    [1., 0., 1., 0., 1.],
+    [0., 1., 0., 1., 0.],
+    [1., 1., 1., 0., 0.],
+    [0., 1., 1., 1., 0.],
+    [1., 0., 0., 1., 1.],
+    [0., 1., 1., 0., 1.],
+    [1., 0., 1., 0., 0.],
+    [0., 1., 1., 1., 0.],
+    [1., 0., 0., 1., 1.],
+    [0., 1., 1., 0., 0.],
+    [1., 0., 1., 0., 1.],
+  ], dtype=torch.float32)
 
-  data_set = LOLSet(x, target)
-  data_loader = data.DataLoader(dataset=data_set, batch_size=4, shuffle=False)
+
+  """
+  100 - 100%
+  x   - 15% 
+
+  """
+
+
+  data_size = x.size(0)
+  train_size = floor(data_size * 70 / 100)
+  val_size = ceil(data_size * 15 / 100)
+  test_size = ceil(data_size * 15 / 100)
+
+
+  print(f'кол-во выборки: {train_size + val_size + test_size}')
+  print(f'кол-во обучающей выборки (70% от всей): {train_size}')
+  print(f'кол-во валидационной выборки (15% от всей): {val_size}')
+  print(f'кол-во тестовой выборки (15% от всей): {test_size}\n')
+
+
+
+  train_set = LOLSet(x[:train_size], target[:train_size])
+  val_set = LOLSet(x[train_size:train_size+test_size], target[train_size:train_size+test_size])
+  test_set = LOLSet(x[train_size+val_size:], target[train_size+val_size:])
+
+  train_loader = data.DataLoader(dataset=train_set, batch_size=4, shuffle=False) # нубская выборка не нуждается в перемешке
+  val_loader = data.DataLoader(dataset=val_set, batch_size=2, shuffle=False)
+  test_loader = data.DataLoader(dataset=test_set, batch_size=4, shuffle=False)
 
   model = NoobMultiLabelModel(4, 5)
   opt = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -80,81 +144,98 @@ def main():
   model.train()
 
   for ep in range(num_ep):
-    losses = 0
-    l_counter = 0
-    for obj, y in data_loader:
-      raw_logit = model(obj)
-      loss = criterion(raw_logit, y)
+    train_loss = 0
+    tl_cnt = 0
+
+    val_loss = 0
+    vl_cnt = 0
+
+    for x, y in train_loader:
+      pred = model(x)
+      loss = criterion(pred, y)
       opt.zero_grad()
       loss.backward()
       opt.step()
 
-      losses += loss.item()
-      l_counter += 1
-    if ep % 200 == 0:
-      print(f'ep [{ep}/{num_ep}] loss mean | {losses / l_counter}')
+      train_loss += loss.item()
+      tl_cnt += 1
+
+    for x, y in val_loader:
+      pred = model(x)
+      val_loss += criterion(pred, y).item()
+      vl_cnt += 1      
+
+
+    if ep % 50 == 0:
+      train_loss_mean = train_loss / tl_cnt
+      val_loss_mean = val_loss / vl_cnt
+  
+      print(f'ep [{ep}/{num_ep}]\tTRAIN loss {train_loss_mean} \t\t VAL loss {val_loss_mean}')
+
+
   model.eval()
 
   with torch.no_grad():
-    raw_logit = model(x)
-    logit = (F.sigmoid(raw_logit) > 0.5).float()
+    for x, target in test_loader:
+      pred = model(x)
+      logit = (F.sigmoid(pred) > 0.5).float()
 
-    label_wise_accuracy = 0
-    exact_match_accuracy = 0
+      label_wise_accuracy = 0
+      exact_match_accuracy = 0
 
-    macro_tp = 0
-    macro_fp = 0
-    macro_tn = 0
-    macro_fn = 0
-    f1_classes = []
-    micro_tp = 0
-    micro_fp = 0
-    micro_tn = 0
-    micro_fn = 0
-    micro_precision = 0
-    micro_recall = 0
-    micro_f1 = 0
-    macro_precision = 0
-    macro_recall = 0
-    macro_f1 = 0
+      macro_tp = 0
+      macro_fp = 0
+      macro_tn = 0
+      macro_fn = 0
+      f1_classes = []
+      micro_tp = 0
+      micro_fp = 0
+      micro_tn = 0
+      micro_fn = 0
+      micro_precision = 0
+      micro_recall = 0
+      micro_f1 = 0
+      macro_precision = 0
+      macro_recall = 0
+      macro_f1 = 0
 
-    label_wise_accuracy = (logit == target).sum() / ((logit.numel() + target.numel()) / 2)   
-    exact_match_accuracy = 1. if label_wise_accuracy == 1. else 0. # c более чем одним sample потреубется изменить вычисление
+      label_wise_accuracy = (logit == target).sum() / ((logit.numel() + target.numel()) / 2)   
+      exact_match_accuracy = 1. if label_wise_accuracy == 1. else 0. # c более чем одним sample потреубется изменить вычисление
 
-    for index_ax2 in range(logit.size(1)):
-      macro_tp = macro_fp = macro_tn = macro_fn = 0
-      macro_precision = macro_recall = 0
+      for index_ax2 in range(logit.size(1)):
+        macro_tp = macro_fp = macro_tn = macro_fn = 0
+        macro_precision = macro_recall = 0
 
-      for index_ax1 in range(logit.size(0)):
-        if logit[index_ax1, index_ax2] == 1.:
-          if target[index_ax1, index_ax2] == 1.:
-            macro_tp += 1
-            micro_tp += 1
-          if target[index_ax1, index_ax2] == 0:
-            macro_fp += 1
-            micro_fp += 1
-        if logit[index_ax1, index_ax2] == 0:
-          if target[index_ax1, index_ax2] == 0:
-            macro_tn += 1
-            micro_tn += 1
-          if target[index_ax1, index_ax2] == 1:
-            macro_fn += 1
-            micro_fn += 1
-          
-      macro_precision = macro_tp / (macro_tp + macro_fp)
-      macro_recall = macro_tp / (macro_tp + macro_fn)
-      f1_classes.append(2 * (macro_precision * macro_recall) / (macro_precision + macro_recall))
+        for index_ax1 in range(logit.size(0)):
+          if logit[index_ax1, index_ax2] == 1.:
+            if target[index_ax1, index_ax2] == 1.:
+              macro_tp += 1
+              micro_tp += 1
+            if target[index_ax1, index_ax2] == 0:
+              macro_fp += 1
+              micro_fp += 1
+          if logit[index_ax1, index_ax2] == 0:
+            if target[index_ax1, index_ax2] == 0:
+              macro_tn += 1
+              micro_tn += 1
+            if target[index_ax1, index_ax2] == 1:
+              macro_fn += 1
+              micro_fn += 1
+            
+        macro_precision = macro_tp / (macro_tp + macro_fp)
+        macro_recall = macro_tp / (macro_tp + macro_fn)
+        f1_classes.append(2 * (macro_precision * macro_recall) / (macro_precision + macro_recall))
 
-    micro_precision = micro_tp / (micro_tp + micro_fp)
-    micro_recall = micro_tp / (micro_tp + micro_fn)
+      micro_precision = micro_tp / (micro_tp + micro_fp)
+      micro_recall = micro_tp / (micro_tp + micro_fn)
 
-    macro_f1 = sum(f1_classes) / len(f1_classes)
-    micro_f1 = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall)
+      macro_f1 = sum(f1_classes) / len(f1_classes)
+      micro_f1 = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall)
 
-    print(f'\nточность модели с точки зрения попадания: {label_wise_accuracy}')
-    print(f'точность модели с точки зрения совпадения: {exact_match_accuracy}')
-    print(f'macro F1 score: {macro_f1}')
-    print(f'micro F1 score: {micro_f1}')
+      print(f'\nточность модели с точки зрения попадания: {label_wise_accuracy}')
+      print(f'точность модели с точки зрения совпадения: {exact_match_accuracy}')
+      print(f'macro F1 score: {macro_f1}')
+      print(f'micro F1 score: {micro_f1}')
 
 if __name__ == "__main__":
   main()
