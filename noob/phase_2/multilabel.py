ce3d024e01a265af0a92203c863cfba6858c2a1f
2fa-auth <sir.jey.daniels@gmail.com>
2026-08-30 17:08:18 +0300

    explanation of the code LOL

--- noob/phase_2/multilabel.py
+++ noob/phase_2/multilabel.py
@@ -4,7 +4,20 @@ import torch.utils.data as data
 import torch.nn as nn
 import torch.nn.functional as F
 
-# многометочная классификация и метрики на бинарной задаче
+"""
+многометочная классификация и метрики (КОСТЫЛЬ):
+  * в этом файле отдается приоритет вычислению метрик.
+    менее приоритетнее сама модель.
+
+  * в дальнейшем файл будет использоваться для эксплуатации 
+    вывода метрик для более глубокого анализа качества модели.
+
+  За основу взята матрица ошибок (T*/ F*):
+    precision = TP + (TP / FP)
+    recall = TP + (TP / FN)
+    F1 = 2 * (precision * recall) / (precision + recall)
+
+"""
 
 
 # -- нубский сет
@@ -19,7 +32,7 @@ class LOLSet(data.Dataset):
   def __len__(self):
     return self.len
 
-
+# -- нубская модель
 class NoobMultiLabelModel(nn.Module):
   def __init__(self, in_features, out_features):
     super().__init__()
@@ -35,6 +48,7 @@ class NoobMultiLabelModel(nn.Module):
 
 
 def main():
+  # -- нубская выборка
   x = torch.tensor([
     [1.0, 0.0, 2.0, 0.5],
     [0.0, 2.0, 1.0, 1.5],
@@ -45,7 +59,6 @@ def main():
     [3.0, 2.0, 2.0, 1.0],
     [1.0, 3.0, 0.5, 2.5],
   ])
-
   target = torch.tensor([
     [1., 0., 1., 0., 0.],
     [0., 1., 1., 0., 1.],
@@ -56,7 +69,6 @@ def main():
     [1., 1., 1., 0., 0.],
     [0., 1., 0., 0., 1.],
   ])
-
 
   data_set = LOLSet(x, target)
   data_loader = data.DataLoader(dataset=data_set, batch_size=4, shuffle=False)
@@ -79,85 +91,70 @@ def main():
 
       losses += loss.item()
       l_counter += 1
-
-    if ep % 100 == 0:
+    if ep % 200 == 0:
       print(f'ep [{ep}/{num_ep}] loss mean | {losses / l_counter}')
-
   model.eval()
 
-  raw_logit = model(x)
-  logit = (F.sigmoid(raw_logit) > 0.5).float()
-  print(f'предсказания модели: \n{logit}')
-  # print(f'logit.shape = {logit.shape}, target.shape = {target.shape}')
+  with torch.no_grad():
+    raw_logit = model(x)
+    logit = (F.sigmoid(raw_logit) > 0.5).float()
 
-  label_wise_accuracy = 0
-  exact_match_accuracy = 0
+    label_wise_accuracy = 0
+    exact_match_accuracy = 0
 
-  macro_tp = 0
-  macro_fp = 0
-  macro_tn = 0
-  macro_fn = 0
-  f1_classes = []
+    macro_tp = 0
+    macro_fp = 0
+    macro_tn = 0
+    macro_fn = 0
+    f1_classes = []
+    micro_tp = 0
+    micro_fp = 0
+    micro_tn = 0
+    micro_fn = 0
+    micro_precision = 0
+    micro_recall = 0
+    micro_f1 = 0
+    macro_precision = 0
+    macro_recall = 0
+    macro_f1 = 0
 
-  micro_tp = 0
-  micro_fp = 0
-  micro_tn = 0
-  micro_fn = 0
+    label_wise_accuracy = (logit == target).sum() / ((logit.numel() + target.numel()) / 2)   
+    exact_match_accuracy = 1. if label_wise_accuracy == 1. else 0. # c более чем одним sample потреубется изменить вычисление
 
-  micro_precision = 0
-  micro_recall = 0
-  micro_f1 = 0
+    for index_ax2 in range(logit.size(1)):
+      macro_tp = macro_fp = macro_tn = macro_fn = 0
+      macro_precision = macro_recall = 0
 
-  macro_precision = 0
-  macro_recall = 0
-  macro_f1 = 0
+      for index_ax1 in range(logit.size(0)):
+        if logit[index_ax1, index_ax2] == 1.:
+          if target[index_ax1, index_ax2] == 1.:
+            macro_tp += 1
+            micro_tp += 1
+          if target[index_ax1, index_ax2] == 0:
+            macro_fp += 1
+            micro_fp += 1
+        if logit[index_ax1, index_ax2] == 0:
+          if target[index_ax1, index_ax2] == 0:
+            macro_tn += 1
+            micro_tn += 1
+          if target[index_ax1, index_ax2] == 1:
+            macro_fn += 1
+            micro_fn += 1
+          
+      macro_precision = macro_tp / (macro_tp + macro_fp)
+      macro_recall = macro_tp / (macro_tp + macro_fn)
+      f1_classes.append(2 * (macro_precision * macro_recall) / (macro_precision + macro_recall))
 
-  print(logit.shape, target.shape)
-  label_wise_accuracy = (logit == target).sum() / ((logit.numel() + target.numel()) / 2)   
-  exact_match_accuracy = 1 if label_wise_accuracy == 1. else 0
-  print(f'точность модели с точки зрения попадания: {label_wise_accuracy}')
-  print(f'точность модели с точки зрения совпадения: {exact_match_accuracy}')
+    micro_precision = micro_tp / (micro_tp + micro_fp)
+    micro_recall = micro_tp / (micro_tp + micro_fn)
 
+    macro_f1 = sum(f1_classes) / len(f1_classes)
+    micro_f1 = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall)
 
-  for index_ax2 in range(logit.size(1)):
-    macro_tp = macro_fp = macro_tn = macro_fn = 0
-    macro_precision = macro_recall = 0
-
-    for index_ax1 in range(logit.size(0)):
-      if logit[index_ax1, index_ax2] == 1.:
-        if target[index_ax1, index_ax2] == 1.:
-          macro_tp += 1
-          micro_tp += 1
-        if target[index_ax1, index_ax2] == 0:
-          macro_fp += 1
-          micro_fp += 1
-      if logit[index_ax1, index_ax2] == 0:
-        if target[index_ax1, index_ax2] == 0:
-          macro_tn += 1
-          micro_tn += 1
-        if target[index_ax1, index_ax2] == 1:
-          macro_fn += 1
-          micro_fn += 1
-        
-    macro_precision = macro_tp / (macro_tp + macro_fp)
-    macro_recall = macro_tp / (macro_tp + macro_fn)
-    f1_classes.append(2 * (macro_precision * macro_recall) / (macro_precision + macro_recall))
-
-  micro_precision = micro_tp / (micro_tp + micro_fp)
-  micro_recall = micro_tp / (micro_tp + micro_fn)
-
-  macro_f1 = sum(f1_classes) / len(f1_classes)
-  micro_f1 = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall)
-
-  print(f'macro F1 score: {macro_f1}')
-  print(f'micro F1 score: {micro_f1}')
+    print(f'\nточность модели с точки зрения попадания: {label_wise_accuracy}')
+    print(f'точность модели с точки зрения совпадения: {exact_match_accuracy}')
+    print(f'macro F1 score: {macro_f1}')
+    print(f'micro F1 score: {micro_f1}')
 
 if __name__ == "__main__":
   main()
-
-# вычисление метрики:
-
-# precision = TP + (TP / FP)
-# recall = TP + (TP / FN)
-# F1 = 2 * (precision * recall) / (precision + recall)
-
